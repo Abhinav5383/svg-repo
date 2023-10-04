f3a4b7a4ae185a1388edc5e9aa405c2e898a78aa
Abhinav5383 <abhinavkumar5383@gmail.com>
2023-10-04 14:07:07 +0530

    PORT TO NEXTJS

--- app/Components/Main/Cards.jsx
+++ app/Components/Main/Cards.jsx
@@ -7,7 +7,7 @@ const Card = ({ iconName, data, createToast, notificationTypes, popupIds, currPo
 
 
   const handleClick = (data) => {
-    setcurrPopup(popupIds.svgDetails);
+    setcurrPopup(popupIds?.svgDetails);
     setSelectedIcon({
       iconName,
       data
@@ -19,7 +19,7 @@ const Card = ({ iconName, data, createToast, notificationTypes, popupIds, currPo
     <div className={`card`} onClick={() => { handleClick(data) }}>
       <div className="card-icon" dangerouslySetInnerHTML={{ __html: data }}></div>
 
-      <div className="card-name">{iconName.replaceAll("-", " ")}</div>
+      <div className="card-name">{iconName?.replaceAll("-", " ")}</div>
     </div>
   )
 }
@@ -42,49 +42,49 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
 
   // Insert an icon into the Trie
   function insertIcon(root, icon) {
-    const name = icon.name.toLowerCase();
-    const tags = icon.tags.toLowerCase();
+    const name = icon?.name?.toLowerCase();
+    const tags = icon?.tags?.toLowerCase();
     let node = root;
 
     for (const char of name) {
-      if (!node.children.has(char)) {
-        node.children.set(char, new TrieNode());
+      if (!node?.children?.has(char)) {
+        node?.children?.set(char, new TrieNode());
       }
-      node = node.children.get(char);
-      node.icons.push(icon);
+      node = node?.children?.get(char);
+      node?.icons?.push(icon);
     }
 
     // Add the icon to the Trie using tags as well
-    for (const tag of tags.split(' ')) {
+    for (const tag of tags?.split(' ')) {
       node = root;
       for (const char of tag) {
-        if (!node.children.has(char)) {
-          node.children.set(char, new TrieNode());
+        if (!node?.children?.has(char)) {
+          node?.children?.set(char, new TrieNode());
         }
-        node = node.children.get(char);
-        node.icons.push(icon);
+        node = node?.children?.get(char);
+        node?.icons?.push(icon);
       }
     }
   }
 
   // Search for icons matching the query using Trie
   function searchIcons(root, query) {
-    const queryLower = query.toLowerCase();
+    const queryLower = query?.toLowerCase();
     const matchingIcons = new Set();
 
     let node = root;
 
     for (const char of queryLower) {
-      if (node.children.has(char)) {
-        node = node.children.get(char);
+      if (node?.children?.has(char)) {
+        node = node?.children?.get(char);
       } else {
         return [...matchingIcons]; // No matching icons for this query
       }
     }
 
     function traverse(node) {
-      node.icons.forEach(icon => matchingIcons.add(icon));
-      for (const [char, childNode] of node.children) {
+      node?.icons?.forEach(icon => matchingIcons?.add(icon));
+      for (const [char, childNode] of node?.children) {
         traverse(childNode);
       }
     }
@@ -96,28 +96,28 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
 
   // Custom sorting function based on relevancy criteria
   function customSort(a, b, query) {
-    const queryLower = query.toLowerCase();
-    const nameLowerA = a.name.toLowerCase();
-    const nameLowerB = b.name.toLowerCase();
-    const tagsLowerA = a.tags.toLowerCase();
-    const tagsLowerB = b.tags.toLowerCase();
+    const queryLower = query?.toLowerCase();
+    const nameLowerA = a?.name?.toLowerCase();
+    const nameLowerB = b?.name?.toLowerCase();
+    const tagsLowerA = a?.tags?.toLowerCase();
+    const tagsLowerB = b?.tags?.toLowerCase();
 
     // Relevance score calculation
     let scoreA = 0;
     let scoreB = 0;
 
     // Priority 1: Exact Match at the Start of Name or Tags
-    if (nameLowerA.startsWith(queryLower) || tagsLowerA.startsWith(queryLower)) {
-      if (nameLowerA.startsWith(queryLower)) {
+    if (nameLowerA?.startsWith(queryLower) || tagsLowerA?.startsWith(queryLower)) {
+      if (nameLowerA?.startsWith(queryLower)) {
         scoreA += 15
       }
       else {
         scoreA += 7
       }
     }
-    if (nameLowerB.startsWith(queryLower) || tagsLowerB.startsWith(queryLower)) {
+    if (nameLowerB?.startsWith(queryLower) || tagsLowerB?.startsWith(queryLower)) {
       scoreB += 10;
-      if (nameLowerB.startsWith(queryLower)) {
+      if (nameLowerB?.startsWith(queryLower)) {
         scoreB += 15
       }
       else {
@@ -126,16 +126,16 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
     }
 
     // Priority 2: Exact Match in Name or Tags
-    if (nameLowerA.includes(queryLower) || tagsLowerA.includes(queryLower)) {
-      if (nameLowerA.includes(queryLower)) {
+    if (nameLowerA?.includes(queryLower) || tagsLowerA?.includes(queryLower)) {
+      if (nameLowerA?.includes(queryLower)) {
         scoreA += 10
       }
       else {
         scoreA += 7
       }
     }
-    if (nameLowerB.includes(queryLower) || tagsLowerB.includes(queryLower)) {
-      if (nameLowerB.includes(queryLower)) {
+    if (nameLowerB?.includes(queryLower) || tagsLowerB?.includes(queryLower)) {
+      if (nameLowerB?.includes(queryLower)) {
         scoreB += 10
       }
       else {
@@ -144,17 +144,17 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
     }
 
     // Priority 3: Partial Match in Name or Tags
-    for (const term of queryLower.split(/[\s\-_]+/)) {
-      if (nameLowerA.includes(term) || tagsLowerA.includes(term)) {
-        if (nameLowerA.includes(term)) {
+    for (const term of queryLower?.split(/[\s\-_]+/)) {
+      if (nameLowerA?.includes(term) || tagsLowerA?.includes(term)) {
+        if (nameLowerA?.includes(term)) {
           scoreA += 3
         }
         else {
           scoreA += 2
         }
       }
-      if (nameLowerB.includes(term) || tagsLowerB.includes(term)) {
-        if (nameLowerB.includes(term)) {
+      if (nameLowerB?.includes(term) || tagsLowerB?.includes(term)) {
+        if (nameLowerB?.includes(term)) {
           scoreB += 3
         }
         else {
@@ -164,10 +164,10 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
     }
 
     // Priority 4: Shorter Name Length
-    if (nameLowerA.length < nameLowerB.length) {
+    if (nameLowerA?.length < nameLowerB?.length) {
       scoreA += 11;
     }
-    if (nameLowerA.length > nameLowerB.length) {
+    if (nameLowerA?.length > nameLowerB?.length) {
       scoreB += 11;
     }
 
@@ -178,7 +178,7 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
     }
 
     // If relevance scores are equal, sort by name length in ascending order as a tiebreaker
-    return nameLowerA.localeCompare(nameLowerB);
+    return nameLowerA?.localeCompare(nameLowerB);
   }
 
   // Function to filter, sort, and return icons based on query and criteria
@@ -191,21 +191,21 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
     }
 
     // Split the query
-    const queryTerms = query.replaceAll(" ", "-").replaceAll("_", "-").split("-");
+    const queryTerms = query?.replaceAll(" ", "-")?.replaceAll("_", "-")?.split("-");
 
     // Search for icons matching the first query term
     const matchingIcons = searchIcons(root, queryTerms[0]);
 
     // Filter icons based on the remaining query terms
-    const filteredIcons = matchingIcons.filter(icon => {
-      const nameLower = icon.name.toLowerCase();
-      const tagsLower = icon.tags.toLowerCase();
-      return queryTerms.slice(1).every(term => nameLower.includes(term) || tagsLower.includes(term));
+    const filteredIcons = matchingIcons?.filter(icon => {
+      const nameLower = icon?.name?.toLowerCase();
+      const tagsLower = icon?.tags?.toLowerCase();
+      return queryTerms?.slice(1)?.every(term => nameLower?.includes(term) || tagsLower?.includes(term));
     });
 
     // Sort the matching icons based on your custom sorting logic
     const matchingIconsArray = [...filteredIcons];
-    matchingIconsArray.sort((a, b) => customSort(a, b, query));
+    matchingIconsArray?.sort((a, b) => customSort(a, b, query));
 
     return matchingIconsArray;
   }
@@ -216,19 +216,19 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
 
   useEffect(() => {
     if (!renderIconsList) return;
-    setMaxPages(renderIconsList.length / pageSize);
+    setMaxPages(renderIconsList?.length / pageSize);
   }, [renderIconsList])
 
 
   useEffect(() => {
 
     if (!query && query !== " ") {
-      setRenderIconsList(data.icons);
+      setRenderIconsList(data?.icons);
     }
 
     else if (query && query !== '') {
 
-      let sortedData = filterAndSortIcons(query, data.icons);
+      let sortedData = filterAndSortIcons(query, data?.icons);
 
       setRenderIconsList(sortedData);
     }
@@ -243,21 +243,22 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
   return (
     <>
 
-      <div className="title">{renderIconsList ? renderIconsList.length : 0} Icons</div>
+      <div className="title">{renderIconsList ? renderIconsList?.length : 0} Icons</div>
 
       <div className='cards'>
         <div className="wrapper">
 
           {renderIconsList &&
-            renderIconsList.map((icon, i) => {
+            renderIconsList?.map((icon, i) => {
 
               if (i >= (page - 1) * pageSize && i < page * pageSize) {
 
                 return (
-                  <React.Fragment key={`${icon.name}-${i}`}>
+                  <React.Fragment key={`${icon?.name}-${i}`
+                  }>
                     < Card
-                      iconName={icon.name}
-                      data={icon.data}
+                      iconName={icon?.name}
+                      data={icon?.data}
                       createToast={createToast}
                       notificationTypes={notificationTypes}
                       popupIds={popupIds}
@@ -273,7 +274,7 @@ const Cards = ({ page, setPage, pageSize, maxPages, setMaxPages, query, createTo
           }
 
         </div>
-      </div>
+      </div >
     </>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
 import "./globals.css";
-
-import data from "./data";
 
 
 
@@ -14,11 +14,50 @@ function App() {
   const [maxPages, setMaxPages] = useState();
   const [query, setQuery] = useState("");
   const [currPopup, setcurrPopup] = useState("");
+  const [data, setData] = useState({ icons: [] });
 
 
   const popupIds = {
     svgDetails: "svgDetails"
-  }
+  };
+
+  const fetchIcons = async () => {
+
+    fetch("/api/data/getdata", {
+      method: "GET"
+    })
+      .then(async (response) => {
+        response = await response.json();
+        if (!response.ok) {
+          return console.log("Error occured");
+        }
+        return response;
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+
+  };
+
+  useEffect(() => {
+    try {
+      let localIcons = JSON.parse(localStorage.getItem("icons"));
+      if (!localIcons?.icons) {
+        fetchIcons();
+      }
+      else {
+        setData(localIcons);
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+
+  }, [])
+
 
 
   return (
