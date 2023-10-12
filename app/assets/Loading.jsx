6944d0f094ab9f7006677054461c9d7d44dc4947
Abhinav5383 <abhinavkumar5383@gmail.com>
2023-10-12 21:48:37 +0530

    Better loading animation

--- app/assets/Loading.jsx
+++ app/assets/Loading.jsx
@@ -10,19 +10,12 @@ const Loading = () => {
 
         <div className="content">
 
-          <div className="icon loader loader--style2" title="1">
-            <svg version="1.1" id="loader-1" x="0px" y="0px"
-              width="40px" height="40px" viewBox="0 0 50 50">
-              <path fill="currentColor" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
-                <animateTransform attributeType="xml"
-                  attributeName="transform"
-                  type="rotate"
-                  from="0 25 25"
-                  to="360 25 25"
-                  dur="0.6s"
-                  repeatCount="indefinite" />
-              </path>
+          <div className="icon loader loader--style2">
+
+            <svg className="spinner" stroke="currentColor" viewBox="0 0 66 66">
+              <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
             </svg>
+
           </div>
 
           <div className="text">Loading...</div>
