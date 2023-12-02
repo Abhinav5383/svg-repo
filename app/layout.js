2bb4798a6b9fca0e1aa8f9ad51c8940f5baea9bb
Abhinav5383 <abhinavkumar5383@gmail.com>
2023-12-02 23:40:13 +0530

    Adsense

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css"
+import Script from "next/script"
 
 export const metadata = {
   title: 'My Icons',
@@ -11,7 +12,12 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" type="image/svg+xml" href="/logo.svg" />
       </head>
-      <body>{children}</body>
+
+      <body>
+        {children}
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8708861413425372"
+          crossorigin="anonymous" />
+      </body>
     </html>
   )
 }
